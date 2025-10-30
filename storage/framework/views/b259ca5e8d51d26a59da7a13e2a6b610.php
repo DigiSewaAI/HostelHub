aedcebabe290761f0a747eead0bc3858861fa936
Parashar <parasharregmi@gmail.com>
2025-10-30 17:14:54 +0545

    home page and public gallery page corrected

--- storage/framework/views/b259ca5e8d51d26a59da7a13e2a6b610.php
+++
@@ -1,4 +0,0 @@
-<img src="<?php echo e(asset('storage/images/logo.png')); ?>" 
-     alt="HostelHub Logo" 
-     class="<?php echo e($logoClass ?? 'w-32 h-10 object-contain'); ?>">
-<?php /**PATH C:\laragon\www\HostelHub\resources\views\components\application-logo.blade.php ENDPATH**/ ?>
