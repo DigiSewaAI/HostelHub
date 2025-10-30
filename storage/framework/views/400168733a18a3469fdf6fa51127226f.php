0685ed1014edd481fb0383321c111ee90a58ffa3
Parashar <parasharregmi@gmail.com>
2025-10-30 13:42:46 +0545

    all cache cleared in local

--- storage/framework/views/400168733a18a3469fdf6fa51127226f.php
+++
@@ -1,3 +0,0 @@
-<img src="<?php echo e(asset('storage/images/logo.png')); ?>" 
-     alt="HostelHub Logo" 
-     class="w-32 h-10 object-contain"><?php /**PATH C:\laragon\www\HostelHub\resources\views\components\logo.blade.php ENDPATH**/ ?>
--- storage/framework/views/cf748cfb9c9ef98bc29eeaef8b33c8ab.php
+++
@@ -1,5 +0,0 @@
-<p>नाम: <?php echo e($data['name']); ?></p>
-<p>इमेल: <?php echo e($data['email']); ?></p>
-<p>फोन: <?php echo e($data['phone'] ?? 'N/A'); ?></p>
-<p>सन्देश: <?php echo e($data['message']); ?></p>
-<?php /**PATH C:\laragon\www\HostelHub\resources\views\emails\contact.blade.php ENDPATH**/ ?>
